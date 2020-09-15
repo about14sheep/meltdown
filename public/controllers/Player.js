08e8d756dd40d580fc949b5c1539b7bc84749bbe
Austin Burger <aburger91@gmail.com>
2020-09-15 10:36:12 -0500

    Stubbed out player controller

---
+++ public/controllers/Player.js
@@ -0,0 +1,54 @@
+import Phaser from 'phaser'
+
+export default class Player extends Phaser.Physics.Arcade.Sprite {
+  constructor(scene, x, y) {
+    super(scene, x, y, 'scientist')
+    this.scene = scene
+    this.isPlayerUsing = false
+    this.lastAnim = null
+    this.velocity = 200
+
+    this.scene.world.enable(this)
+    this.scene.add.existing(this)
+
+    const { A, D, W, S } = Phaser.Input.Keyboard.KeyCodes
+    this.keys = this.scene.input.keyboard.addKeys({
+      left: A,
+      right: D,
+      up: W,
+      down: S
+    })
+  }
+
+  preUpdate(time, delta) {
+    super.preUpdate(time, delta)
+
+    const keys = this.keys
+    let currentAnimKey = 'idle'
+
+    if (keys.left.isDown) {
+      this.body.setVelocityX(-this.velocity)
+      this.setFlipX(true)
+    } else if (keys.right.isDown) {
+      this.body.setVelocityX(this.velocity)
+      this.setFlipX(false)
+    }
+
+    if (keys.up.isDown) {
+      this.body.setVelocityY(this.velocity)
+    } else if (keys.down.isDown) {
+      this.body.setVelocityY(-this.velocity)
+    }
+
+    if (this.body.velocity != 0) {
+      currentAnimKey = 'walking'
+    } else {
+      currentAnimKey = 'idle'
+    }
+
+    if (this.lastAnim !== currentAnimKey) {
+      this.lastAnim = currentAnimKey
+      this.anims.play(currentAnimKey, true)
+    }
+  }
+}
